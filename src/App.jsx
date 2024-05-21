b51092e27b13b75da4915670b8bf70a420e9efa6
Erik <eait84344@gmail.com>
2024-05-21 17:58:07 +0400

    added component for Film lists and called in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import FilmList from './components/film-lists/FilmList'
 import Header from './components/header'
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <FilmList />
     </div>
   )
 }
---
+++ src/components/film-lists/FilmList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FilmList() {
+  return (
+    <div className='film-list'>
+
+    </div>
+  )
+}
