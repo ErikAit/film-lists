8c4aaddaf9ab7c33370a4d863c2c5a3f2ffff237
Erik <eait84344@gmail.com>
2024-05-22 16:44:00 +0400

    added some styles for the lists

--- src/components/film-lists/lists/List.jsx
+++ src/components/film-lists/lists/List.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function List({ film, handleSelectMovie, isSelected }) {
   return (
-    <div className={`list text-white cursor-pointer ${isSelected === true ? 'border-[1px] border-white' : ''}`} onClick={() => handleSelectMovie(film)}>
+    <div className={`list text-white cursor-pointer ${isSelected === true ? 'border-[1px] border-white font-[600]' : ''} rounded-[9px] p-3`} onClick={() => handleSelectMovie(film)}>
       <img
         alt="film-image"
         src={`https://image.tmdb.org/t/p/w500/${film.backdrop_path}`}
-        className='w-[250px] h-[140px] rounded-md'
+        className='w-[250px] h-[140px] rounded-md object-cover'
       />
       <div>
         {film.original_title}
