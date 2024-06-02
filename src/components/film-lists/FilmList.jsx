1d33713000774ffdbb6de8c2d39889e55519f9dc
Erik <eait84344@gmail.com>
2024-06-02 20:33:48 +0400

    .

--- src/components/film-lists/FilmList.jsx
+++ src/components/film-lists/FilmList.jsx
@@ -1,64 +1,68 @@
-import React, { useEffect, useState, useRef, createRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import List from './lists/List';
+import './lists/list-css/filmlist.css';
 
 export default function FilmList() {
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(0);
-  const [border, setBorder] = useState(false);
-  const movieRefs = useRef([]);
+  const listRef = useRef(null);
 
   useEffect(() => {
-    fetch('https://api.themoviedb.org/3/movie/popular?api_key=877a0b91e805fb63c2bb7e48097344dc')
-      .then((res) => res.json())
-      .then((data) => {
-        setMovies(data.results);
-        movieRefs.current = data.results.map(() => createRef());
-      });
+    const fetchData = async () => {
+      const response1 = await fetch('https://api.themoviedb.org/3/search/movie?api_key=877a0b91e805fb63c2bb7e48097344dc&query=batman&page=1');
+      const data1 = await response1.json();
+
+      const response2 = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=d91b4b2e8fb2707acd809975c49bcf87&query=');
+      const data2 = await response2.json();
+
+      setMovies([...data1.results, ...data2.results]);
+    };
+
+    fetchData();
   }, []);
 
   useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (['ArrowRight', 'ArrowLeft', 'ArrowDown', 'ArrowUp'].includes(event.key)) {
-        event.preventDefault();
-        if (event.key === 'ArrowRight') {
-          setSelectedMovie(index => Math.min(index + 1, movies.length - 1));
-        } else if (event.key === 'ArrowLeft') {
-          setSelectedMovie(index => Math.max(index - 1, 0));
-        } else if (event.key === 'ArrowDown') {
-          setSelectedMovie(index => Math.min(index + Math.ceil(movies.length / 2), movies.length - 1));
-        } else if (event.key === 'ArrowUp') {
-          setSelectedMovie(index => Math.max(index - Math.ceil(movies.length / 2), 0));
+    const handleSelectFilm = (e) => {
+      if (['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'].includes(e.key)) {
+        e.preventDefault();
+        if (e.key === 'ArrowRight') {
+          setSelectedMovie((prevSelectedMovie) => Math.min(prevSelectedMovie + 1, movies.length - 1));
+        } else if (e.key === 'ArrowLeft') {
+          setSelectedMovie((prevSelectedMovie) => Math.max(prevSelectedMovie - 1, 0));
+        } else if (e.key === 'ArrowUp') {
+          setSelectedMovie((prevSelectedMovie) => Math.max(prevSelectedMovie - 10, 0));
+        } else if (e.key === 'ArrowDown') {
+          setSelectedMovie((prevSelectedMovie) => Math.min(prevSelectedMovie + 10, movies.length - 1));
         }
       }
     };
 
-    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keydown', handleSelectFilm);
 
     return () => {
-      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keydown', handleSelectFilm);
     };
-  }, [movies]);
+  }, [movies.length]);
 
   useEffect(() => {
-    if (movieRefs.current[selectedMovie]) {
-      movieRefs.current[selectedMovie].current.scrollIntoView({
-        behavior: 'smooth',
-      });
+    if (listRef.current) {
+      const selectedListItem = listRef.current.querySelector('.selected');
+      if (selectedListItem) {
+        selectedListItem.scrollIntoView({
+          behavior: 'smooth',
+          inline: 'start',
+        });
+      }
     }
   }, [selectedMovie]);
 
-  const handleSelectMovie = (index) => {
-    setSelectedMovie(index);
-  }
-
   return (
     <div className='film-list my-[10px]'>
-
       <div className="selected-movie my-[40px]">
         {movies.length > 0 && (
           <div className='pl-[25px]'>
             <div
-              className="film-bg rounded-md w-[1300px] h-[600px] bg-cover relative"
+              className="film-bg rounded-md w-[1000px] h-[400px] bg-cover relative"
               style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w780/${movies[selectedMovie].backdrop_path})` }}
             >
               <div className="absolute inset-0 bg-gradient-to-r from-black to-transparent"></div>
@@ -68,25 +72,42 @@ export default function FilmList() {
         )}
       </div>
 
-      <div className="movies-container">
-        <div className="movies w-full h-[290px] my-3 overflow-hidden overflow-x-hidden grid grid-cols-10 px-[20px] gap-[20px] gap-x-[300px]" >
-          {movies.slice(0, Math.ceil(movies.length / 2)).map((info, index) => (
+      <div className="movies-container flex flex-col items-center" ref={listRef}>
+        <div className="movies-row">
+          {movies.slice(0, 10).map((info, index) => (
             <List
-              ref={movieRefs.current[index]}
               film={info}
               isSelected={index === selectedMovie}
-              handleSelectMovie={() => handleSelectMovie(index)}
               key={info.id}
             />
           ))}
         </div>
-        <div className="movies w-full h-[290px] my-3 overflow-hidden overflow-x-hidden grid grid-cols-10 px-[20px] gap-[20px] gap-x-[300px]" >
-          {movies.slice(Math.ceil(movies.length / 2)).map((info, index) => (
+
+        <div className="movies-row">
+          {movies.slice(10, 20).map((info, index) => (
             <List
-              ref={movieRefs.current[index + Math.ceil(movies.length / 2)]}
               film={info}
-              isSelected={(index + Math.ceil(movies.length / 2)) === selectedMovie}
-              handleSelectMovie={() => handleSelectMovie(index + Math.ceil(movies.length / 2))}
+              isSelected={index + 10 === selectedMovie}
+              key={info.id}
+            />
+          ))}
+        </div>
+
+        <div className="movies-row">
+          {movies.slice(20, 30).map((info, index) => (
+            <List
+              film={info}
+              isSelected={index + 20 === selectedMovie}
+              key={info.id}
+            />
+          ))}
+        </div>
+
+        <div className="movies-row">
+          {movies.slice(30, 40).map((info, index) => (
+            <List
+              film={info}
+              isSelected={index + 30 === selectedMovie}
               key={info.id}
             />
           ))}
--- src/components/film-lists/lists/List.jsx
+++ src/components/film-lists/lists/List.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-
 import './list-css/list.css';
 
-const List = React.forwardRef(({ film, handleSelectMovie, isSelected }, ref) => (
+const List = ({ film, isSelected }) => (
   <div
-    ref={ref}
-    className={`list w-[300px] text-white cursor-pointer ${isSelected ? 'selected font-[600]' : ''} rounded-[9px] p-3`}
-    onClick={() => handleSelectMovie(film)}
+    className={`list font-[600] text-[15px] w-[300px] h-[240px] text-white cursor-pointer ${isSelected ? 'selected' : ''} rounded-[9px] p-3`}
   >
     <img
       alt="film-image"
       src={`https://image.tmdb.org/t/p/w500/${film.backdrop_path}`}
       className='w-[400px] h-[140px] rounded-md object-cover'
     />
-    <div>
-      {film.original_title}
-    </div>
-    <div>
-      {film.release_date}
+
+    <div className='mt-[10px]'>
+      <div>
+        {film.original_title}
+      </div>
+      <div>
+        {film.release_date}
+      </div>
     </div>
   </div>
-));
+);
 
 export default List;
